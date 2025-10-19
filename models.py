19f3eb11689d5d6678268c12baa346507ee70fd5
Peigi-coder <kkuruliukevgeniy@gmail.com>
2025-10-19 11:47:22 +0200

    Create Book model in models.py
    Define Book model with attributes and to_dict method.

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Book(db.Model):
+__tablename__ = 'books'
+id = db.Column(db.Integer, primary_key=True)
+title = db.Column(db.String(255), nullable=False)
+author = db.Column(db.String(255), nullable=False)
+published_date = db.Column(db.Date)
+pages = db.Column(db.Integer, default=0)
+
+
+def to_dict(self):
+return {
+'id': self.id,
+'title': self.title,
+'author': self.author,
+'published_date': self.published_date.isoformat() if self.published_date else None,
+'pages': self.pages
+}
